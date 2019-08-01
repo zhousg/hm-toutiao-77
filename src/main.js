3587b88965e836dad83ed38999ba66f8a64f6ffa
zhousg <zhoushugang@itcast.cn>
2019-08-01 11:55:23 +0800

    使用element-ui&使用vue-router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// @ 是某一个路径别名  src路径别名  在vue-cli创建的项目下才可是呀
+// 目录下是有默认索引文件 index.js 就是索引文件  .js .vue .json
+import router from '@/router'
+
 // 控制台提示信息 开发环境来提示  详细一些
 Vue.config.productionTip = false
 
 // main.js 的职责
 // 1. 职责：导入项目需要依赖的资源（js包 css文件 其他资源）
 // 2. 职责：创建一个根实例
-
+Vue.use(ElementUI)
 // 根实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+// 定义router对象  导出给main.js使用
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  // 路由规则配置
+  routes: []
+})
+
+export default router
