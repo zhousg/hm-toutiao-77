2567c525b62049f09fc1ef964948e923079ebc40
zhousg <zhoushugang@itcast.cn>
2019-08-01 10:28:02 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 控制台提示信息 开发环境来提示  详细一些
 Vue.config.productionTip = false
 
+// main.js 的职责
+// 1. 职责：导入项目需要依赖的资源（js包 css文件 其他资源）
+// 2. 职责：创建一个根实例
+
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
