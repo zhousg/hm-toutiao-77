2b40fa03e26f3d4506db14714b0e800d6f949421
zhousg <zhoushugang@itcast.cn>
2019-08-02 09:23:47 +0800

    绘制表单

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import '@/style/index.less'
+
 // @ 是某一个路径别名  src路径别名  在vue-cli创建的项目下才可是呀
 // 目录下是有默认索引文件 index.js 就是索引文件  .js .vue .json
 import router from '@/router'
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,59 @@
 <template>
-  <div class='container'>Login</div>
+  <div class='container'>
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt="">
+      <!-- 表单 -->
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px;"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  // 背景图定位 / 背景图尺寸 contain 等比缩放完整在容器内显示  cover  等比缩放完全铺面容器
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover
+}
+.my-card{
+  width: 400px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-60%);
+  img{
+    display: block;
+    width: 200px;
+    margin: 0 auto 30px;
+  }
+}
+</style>
