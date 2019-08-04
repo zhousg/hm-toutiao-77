ce82e58216f4a67a373d2281304cb9c744e5b884
zhousg <zhoushugang@itcast.cn>
2019-08-04 12:47:16 +0800

    请求拦截&响应拦截

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,45 @@
 // 配置一个axios  导出一个配置好的axios
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
 
 // 进行配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
-axios.defaults.headers = {
-  Authorization: `Bearer ${store.getUser().token}`
-}
+// 只会执行一次
+// axios.defaults.headers = {
+//   Authorization: `Bearer ${store.getUser().token}`
+// }
+// 在每次请求之前，获取token信息，追加在headers中
+// 请求拦截器：在每次请求前 做某一些事情
+axios.interceptors.request.use((config) => {
+  // 修改config
+  config.headers = {
+    Authorization: `Bearer ${store.getUser().token}`
+  }
+  return config
+}, (error) => {
+  // 错误处理
+  return Promise.reject(error)
+})
+// 响应拦截器：在每次响应后 做某一些事情
+axios.interceptors.response.use((res) => {
+  // 成功的时候做一些事情
+  return res
+}, (err) => {
+  // 失败的时候做一些事情
+  // 获取响应状态码 err对象包含响应对象  err.response
+  // 响应对象中包含状态码  err.response.status
+  if (err.response.status === 401) {
+    // 跳转到登录页面 this.$router.push('/login')
+    // 方式1：
+    // $router.push('/login') 其实 地址栏改成  #/login
+    // location.hash = '#/login'
+    // 方式2：
+    // 此时没有vue实例，$router对象获取不到，通过router实例直接使用
+    router.push('/login')
+  }
+  return Promise.reject(err)
+})
 
 // 进行导出
 export default axios
