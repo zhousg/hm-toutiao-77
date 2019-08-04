5f34cc2b8f38d3439bc6a4380ad9c4c77b89c563
zhousg <zhoushugang@itcast.cn>
2019-08-04 09:05:38 +0800

    导航菜单路由功能

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
+import Article from '@/views/article'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,8 @@ const router = new VueRouter({
       path: '/',
       component: Home,
       children: [
-        { path: '/', name: 'welcome', component: Welcome }
+        { path: '/', name: 'welcome', component: Welcome },
+        { path: '/article', name: 'article', component: Article }
       ]
     }
   ]
