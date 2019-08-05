12a9178dff90c004bd3001776b568cdf0c0c3c53
zhousg <zhoushugang@itcast.cn>
2019-08-05 09:09:34 +0800

    login改成async await 写法

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -58,26 +58,35 @@ export default {
   methods: {
     login () {
       // 对整个表单进行校验
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate(async valid => {
         if (valid) {
-          // 请求登录接口
-          this.$http
-            .post(
-              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
-              this.loginForm
-            )
-            .then(res => {
-              // res 响应对象   包含响应主体
-              // console.log(res.data)
-              // 存储用户信息
-              store.setUser(res.data.data)
-              // 跳转去首页
-              this.$router.push('/')
-            })
-            .catch(() => {
-              // 错误提示提示
-              this.$message.error('手机号或验证码错误')
-            })
+          // // 请求登录接口
+          // this.$http
+          //   .post(
+          //     'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+          //     this.loginForm
+          //   )
+          //   .then(res => {
+          //     // res 响应对象   包含响应主体
+          //     // console.log(res.data)
+          //     // 存储用户信息
+          //     store.setUser(res.data.data)
+          //     // 跳转去首页
+          //     this.$router.push('/')
+          //   })
+          //   .catch(() => {
+          //     // 错误提示提示
+          //     this.$message.error('手机号或验证码错误')
+          //   })
+
+          // async&await使用  怎么处理错误 try{}catch(e){} 捕获异常
+          try {
+            const { data: { data } } = await this.$http.post('authorizations', this.loginForm)
+            store.setUser(data)
+            this.$router.push('/')
+          } catch (e) {
+            this.$message.error('手机号或验证码错误')
+          }
         }
       })
     }
