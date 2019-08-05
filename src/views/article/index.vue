25e4908aaee111d28fe5baf82d8b051de3929ab6
zhousg <zhoushugang@itcast.cn>
2019-08-05 10:38:13 +0800

    内容管理-筛选布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,11 +1,65 @@
 <template>
-  <div class='container'>Article</div>
+  <div class="container">
+    <!-- 筛选条件 -->
+    <el-card>
+      <div slot="header">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-form label-width="80px" size="small">
+        <el-form-item label="状态：">
+          <el-radio-group v-model="reqParams.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <el-select v-model="reqParams.channel_id" placeholder="请选择">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期：">
+          <el-date-picker
+            v-model="dateArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 筛选结果 -->
+  </div>
 </template>
 
 <script>
 export default {
-  created () {
-    this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res => console.log(res.data))
+  data () {
+    return {
+      // 筛选项表单数据  提交给后台参数
+      reqParams: {
+        status: null,
+        channel_id: null
+      },
+      // 频道下拉选项数据
+      channelOptions: [{ value: 1, label: 'js' }],
+      // 日期数据
+      dateArr: []
+    }
   }
 }
 </script>
